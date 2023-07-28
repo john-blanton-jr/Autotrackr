112dd83a69f11d032c06e445a8fc68bcafeda641
Franz Espinosa <espinosa.franz@gmail.com>
2023-07-28 14:24:25 -0400

    fixed service urls

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -4,7 +4,7 @@ from .views import appt_list, tech_list, appt_details, show_tech
 urlpatterns = [
 
     path('technicians/', tech_list, name='tech_list'),
-    path('technician/<int:id>/', show_tech, name='show_tech'),
+    path('technicians/<int:id>/', show_tech, name='show_tech'),
     path('appointments/', appt_list, name='appointment_list'),
     path('appointments/<int:id>', appt_details, name='appointment_details'),
 
